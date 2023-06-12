e3b2b556e2057425dd459b47424e37ebe5bb2229
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-06-11 23:40:56 -0400

    Initial Commit

---
+++ Name Game Culminating/Name_Game_CulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Name_Game_CulminatingApp.swift
+//  Name Game Culminating
+//
+//  Created by Quin Taylor on 2023-06-11.
+//
+
+import SwiftUI
+
+@main
+struct Name_Game_CulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
